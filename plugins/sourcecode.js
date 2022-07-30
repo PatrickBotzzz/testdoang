2f8a769c0f6eb80a29fc5079e650438aa11d3570
PatrickBotzzz <rasyarmdhn20@gmail.com>
2022-07-30 23:04:03 +0700

    wangsaff

---
+++ plugins/sourcecode.js
@@ -0,0 +1,26 @@
+let handler = async (m, { conn }) => {
+	
+	// Link Script jgn di ganti ya pliss
+    // Kalo mau masukin github kamu tinggal add aja link githubnya tapi jangan di hapus sumber script!
+    // Jangan Ngeyell..
+    // Kalo ngeyel gk bakal gw up sc lgi.
+    
+    let txt = `
+Bot ini menggunakan script github
+
+FAMILY-MD
+
+pliss follow dan kasih ✨⭐⭐🌟 ya pak!
+
+My Github: ???
+
+`
+     conn.reply(m.chat, txt, m)
+}
+handler.help = ['sourcecode']
+handler.tags = ['info']
+handler.command = /^(sc(ript(bot)?)?|sourcecode)$/i
+
+module.exports = handler
+
+
